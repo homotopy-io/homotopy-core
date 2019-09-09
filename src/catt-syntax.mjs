757affd5a125466e5a4ad7f0c4887ff7fd5a76af
Eric Finster <ericfinster@gmail.com>
2019-09-09 19:08:58 +0200

    add composition code generation

---
+++ src/catt-syntax.mjs
@@ -0,0 +1,148 @@
+//
+// syntax.mjs - Catt syntax definitions
+//
+
+
+// Types
+
+export class CattType { constructor() { } }
+
+export class CattObject extends CattType {
+
+    constructor() { super() }
+
+}
+
+export class CattArrow extends CattType {
+
+    constructor(src, tgt) { 
+        // _assert(src instanceof CattTerm)
+        // _assert(src instanceof CattTerm)
+
+	super()
+        this.src = src
+        this.tgt = tgt
+    }
+
+}
+
+// Terms
+
+export class CattTerm { constructor() { } }
+
+export class CattVar extends CattTerm {
+
+    constructor(ident) {
+	super()
+        this.ident = ident
+    }
+
+}
+
+export class CattSubst extends CattTerm {
+
+    constructor(tm, subst) {
+	super()
+        this.tm = tm
+        this.subst = subst
+    }
+
+}
+
+// Definitions
+
+export class CattDefn { constructor() {} }
+
+export class CattCoh extends CattDefn {
+
+    constructor(ident, pd, ty) {
+	super()
+        this.ident = ident
+        this.pd = pd
+        this.ty = ty
+    }
+
+}
+
+export class CattLet extends CattDefn {
+
+    constructor(ident, ctx, ty, tm) {
+	super()
+        this.ident = ident
+        this.ctx = ctx
+        this.ty = ty
+        this.tm = tm
+    }
+
+}
+
+// Pretty printing
+
+export function prettyPrintTerm(tm) {
+    
+    if (tm instanceof CattVar) {
+	return tm.ident;
+    } else if (tm instanceof CattSubst) {
+
+	// A substitution is just a list of terms, so iterate
+	// over the list, printing the terms, then apply them
+	// to the base term.
+
+	var substStr = "";
+
+	tm.subst.forEach(function(el) {
+	    substStr += " " + prettyPrintTerm(el) 
+	})
+	
+	return prettyPrintTerm(tm.tm) + substStr
+
+    } else {
+	return "unknown_term";
+    }
+    
+}
+    
+export function prettyPrintType(ty) {
+    
+    if (ty instanceof CattObject) {
+	return "*";
+    } else if (ty instanceof CattArrow) {
+	return prettyPrintTerm(ty.src) + " -> " + prettyPrintTerm(ty.tgt);
+    } else {
+	return "unknown_type";
+    }
+    
+}
+
+export function prettyPrintCtx(ctx) {
+    
+    // Generate the context string
+    var ctxStr = "";
+    var cnt = 0;
+    
+    // Assumes el has "ident" and "type" fields ...
+    ctx.forEach(function(el) {
+        if (cnt > 0) { ctxStr += " " };
+	ctxStr += "(" + el.ident + " : " + prettyPrintType(el.type) + ")";
+        cnt++;
+    })
+    
+    return ctxStr;
+    
+}
+    
+export function prettyPrintDef(def) {
+    
+    if (def instanceof CattCoh) {
+	return "coh " + def.ident + " " + prettyPrintCtx(def.pd) +
+	    " : " + prettyPrintType(def.ty);
+    } else if (def instanceof CattLet) {
+	return "let " + def.ident + prettyPrintCtx(def.ctx) +
+	    " : " + prettyPrintType(def.ty) +
+	    " := " + prettyPrintTerm(def.tm);
+    } else {
+	return "unknown_def";
+    }
+    
+}
+
--- src/catt.js
+++ src/catt.js
@@ -1,266 +1,102 @@
-//import { assert } from "@firebase/util";
-import { _assert } from "~/util/debug";
-import { Diagram } from "~/diagram";
+//
+// catt.js - catt javascript routines
+//
 
+import { prettyPrintCtx, prettyPrintDef, CattObject, CattArrow, CattVar, CattSubst, CattCoh } from './catt-syntax.mjs';
 
-export class CattType {
+export function generateGridComp(dims) {
 
-    constructor() { }
-  
-}
+    // Initial context consists of a single object
+    var ctx = [{ ident : "x0", type : new CattObject }]
 
-export class CattObject extends CattType {
-
-    constructor() { super() }
-
-}
-
-export class CattArrow extends CattType {
-
-    constructor(src, tgt) { 
-        _assert(src instanceof CattType)
-        _assert(src instanceof CattType)
-
-	super()
-        this.src = src
-        this.tgt = tgt
-    }
-
-}
-
-export class CattTerm {
-
-}
-
-export class CattVar extends CattTerm {
-
-    constructor(ident) {
-	super()
-        this.ident = ident
-    }
-
-}
-
-export class CattSubst extends CattTerm {
-
-    constructor(tm, subst) {
-	super()
-        this.tm = tm
-        this.subst = subst
-    }
-
-}
-
-export class CattDefn {
-    constructor() {}
-}
-
-export class CattCoh extends CattDefn {
-
-    constructor(ident, pd, ty) {
-	super()
-        this.ident = ident
-        this.pd = pd
-        this.ty = ty
-    }
-
-}
-
-export class CattLet extends CattDefn {
-
-    constructor(ident, ctx, ty, tm) {
-	super()
-        this.ident = ident
-        this.ctx = ctx
-        this.ty = ty
-        this.tm = tm
-    }
-
-}
-
-export class PrettyPrinter {
-
-    prettyPrintTerm(tm) {
-	if (tm instanceof CattVar) {
-	    return tm.ident;
-	} else if (tm instanceof CattSubst) {
-
-	    // A substitution is just a list of terms, so iterate
-	    // over the list, printing the terms, then apply them
-	    // to the base term.
-
-	    let subStr = "";
-
-	    tm.subst.forEach(function(el) {
-		substStr += " " + prettyPrintTerm(el) 
-	    })
-	    
-	    return prettyPrintTerm(tm.tm) + subStr
-
-	} else {
-	    return "unknown_term";
-	}
-    }
+    var paramsLeft = [];
+    var paramsRight = [];
     
-    prettyPrintType(ty) {
-	if (ty instanceof CattOb) {
-	    return "*";
-	} else if (ty instanceof CattArrow) {
-	    return prettyPrintTerm(ty.src) + " -> " + prettyPrintTerm(ty.tgt);
-	} else {
-	    return "unknown_type";
-	}
-    }
+    // Okay, fantastic.  This looks good.  So the next idea is to
+    // record the source and target arguments so that we can do the
+    // whole coherence....
 
-    prettyPrintCtx(ctx) {
-	// Generate the context string
-	let ctxStr = "";
-	let varCount = 0;
-	ctx.forEach(function(el) {
-	    ctxStr += " (" + "x" + varCount + " : " + prettyPrintType(el) + ")"
-	})
-	return ctxStr;
-    }
+    // How to do this? We would like to mark the codimension 1
+    // entry and exit points.
     
-    prettyPrintDef(def) {
-	if (def instanceof CattCoh) {
-	    return "coh " + def.ident + prettyPrintCtx(def.pd) +
-		" : " + prettyPrintType(def.ty);
-	} else if (def instanceof CattLet) {
-	    return "let " + def.ident + prettyPrintCtx(def.ctx) +
-		" : " + prettyPrintType(def.ty) +
-		" := " + prettyPrintTerm(def.tm);
-	} else {
-	    return "unknown_def";
-	}
-    }
-    
-}
+    function gridLocal(ds, offsets, tgtType) {
 
-export class TypeChecker {
+        // console.log("----------------");
+        // console.log("In dimension: " + offsets.length);
+        // console.log("Dimension profile: " + ds);
+        // console.log("Offset profile: " + offsets);
+        
+        // When there are no dimension specs left, we done
+        if (ds.length == 0) {
+            // console.log("No work in this dimension");
+            return;
+        }
 
-    // Typechecking routines for Catt Syntax
+        var markParams = ds.length == 1;
+        
+        for (var i=0;i<ds[0];i++) {
 
-}
+            // Generate the variable prefix string
+            var offsetStr = "x"
+            offsets.forEach(function(off) { offsetStr += off });
 
-export class GridGenerator {
+            var srcIdent = offsetStr + i;
+            var tgtIdent = offsetStr + (i+1);
+            var flrIdent = srcIdent + "0";
+            var newTgtType = new CattArrow(new CattVar(srcIdent), new CattVar(tgtIdent));
+            
+            // New source context element
+            ctx.push({ ident : tgtIdent, type : tgtType })
+            ctx.push({ ident : flrIdent, type : newTgtType })
 
-    // dims: an array of integers [k_i] with k_i > 0 for all i.
-    //       these specify the number of cells glued along the
-    //       faces of dimension i.
-    gen_grid_context(dims) {
+            if (i == 0 && markParams) {
+                // console.log("Found left parameter: " + offsetStr + i);
+                paramsLeft.push(new CattVar(srcIdent));
+            }
+
+            if (i == ds[0] - 1 && markParams) {
+                // console.log("Found right parameter: " + offsetStr + (i+1));
+                paramsRight.push(new CattVar(tgtIdent));
+            }
+            
+            // Recursive call
+            gridLocal(ds.slice(1), offsets.concat([i]), newTgtType);
+
+        }
         
     }
+
+    gridLocal(dims, [], new CattObject);
+
+    console.log("----------------");
+    // console.log(prettyPrintCtx(ctx));
+    // console.log("Left Parameters: " + paramsLeft);
+    // console.log("Right Parameters: " + paramsRight);
+
+    console.log("Dimension profile: " + dims);
+    
+    var gridPrefStr = "grid";
+    dims.slice(0,dims.length - 1).forEach(function(dim) {
+        console.log("Adding dim: " + dim);
+        gridPrefStr += dim;
+    });
+
+    console.log("Grid string: " + gridPrefStr);
+    
+    var gridStr = gridPrefStr + dims[dims.length-1];
+
+    var srcTyp = new CattSubst(new CattVar(gridPrefStr), paramsLeft);
+    var tgtTyp = new CattSubst(new CattVar(gridPrefStr), paramsRight);
+    var retTyp = new CattArrow(srcTyp, tgtTyp);
+    var coh = new CattCoh(gridStr, ctx, retTyp);
+    
+    return coh;
     
 }
 
-export class Interpreter {
+var coh = generateGridComp([3,1,2,2]);
 
-    contructor() {
-	
-    	this.env = { };
+console.log(prettyPrintDef(coh));
+console.log("Finished.");
 
-	env.id = new CattCoh("id", [ new CattObj ], new CattArrow(new CattVar(0), new CattVar(0)) )
-	
-    }
-
-    
-    interpret(sig, dia) {
-    	return this.interpret_rec(sig, dia, dia.n, 0);
-    }
-
-    // interpret : Sig -> Diagram -> Nat -> Nat -> CattLet
-    interpret_rec(sig, dia, n /* diagram dimension */, k /* promised to have unique content at k levels */) {
-
-        _assert(dia.n == n);
-        // _assert to verify k
-
-        if (n == 1) {
-            return this.interpret_1(sig, dia, k);
-        }
-
-    }
-
-    interpret_1(sig, dia, k) {
-
-        // if (k == 1) {
-        //     return interpret_1_usc(sig, dia);
-        // }
-
-        // dia_l = dia.data.length;
-
-        /* explode */
-
-        let components = [];
-        let source = dia.source;
-        let n = dia.n;
-        for (let i=0; i<dia.data.length; i++) {
-            let data = dia.data[i];
-            components.push(new Diagram({ n, source, data }));
-            source = source.rewrite(data);
-        }
-
-        // interpret
-        let interpretations = components.map(d => this.interpret_1_usc(sig, d, 1));
-
-        // paste
-        let coh = this.gen_arrow_comp(dia_l);
-
-        // return final interpretation
-        return;
-
-    }
-
-    interpret_1_usc(sig, dia) {
-
-    	let source_type = dia.data[0].forward_limit.source_type;
-    	let central_type = dia.data[0].forward_limit.target_type;
-    	let target_type = dia.data[0].backward_limit.source_type;
-
-    	if (source_type == central_type) {
-    	    _assert(central_type == target_type);
-    	    // Identity-like
-
-	    // let def = new CattLet(central_type, ???, ???, ???);
-
-	    return; // Return an identity coherence
-	    
-    	} else {
-    		// Algebraic 1-generator
-    		return; // return the name of central_type
-    	}
-        
-
-    }
-
-    gen_arrow_comp_ctx(n) {
-        _assert(isNatural(n));
-        if (n == 0) return [new CattObject()];
-        let r = this.gen_arrow_comp_ctx(n - 1);
-        let y;
-        if (n == 1) {
-	    y = 0
-        } else {
-	    y = r.length - 2
-        }
-        let z = new CattObject();
-        let arr = new CattArrow(new CattVar(y), new CattVar(r.length));
-        r.push(z, arr);
-        return r;
-    }
-
-    // gen_arrow_comp : Nat -> CattCoh
-    gen_arrow_comp(n) {
-        let ctx = gen_arrow_comp_ctx(n)
-        let tgt_tm;
-        if (n == 0) {
-            tgt_tm = 0;
-        } else {
-            tgt_tm = ctx.length - 2;
-        }
-        let coh = new CattCoh("comp_1_" + n, ctx, new CattArrow(new CattVar(0), new CattVar(tgt_tm)));
-        return coh;
-    }
-
-}
 
