cb01ca18333785a9c9c75733b94248c95024f546
Jamie Vicary <jamievicary@gmail.com>
2019-09-16 17:42:20 +0100

    stuff

--- src/catt.js
+++ src/catt.js
@@ -4,6 +4,9 @@
 
 import { dimOf, prettyPrintCtx, prettyPrintTerm, prettyPrintDef, CattObject, CattArrow, CattVar, CattSubst, CattCoh, CattLet } from './catt-syntax.js';
 
+let testarr = [[[1,2],[3,4],[5,6]],[[7,8],[9,10],[11,12]],[[13,14],[15,16],[17,18]],[[19,20],[21,22],[23,24]]];
+
+
 // Generate a d-dimensional identity coherence
 export function generateIdentity(d) {
 
